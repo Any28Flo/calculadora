8aec8410455657eafd82ef5518f19dfd2f614f89
“Erika <foglzerika@gmail.com>
2017-03-24 09:43:50 -0600

    Cambios en el display

--- js/operaciones.js
+++ js/operaciones.js
@@ -1,37 +1,39 @@
 function enPantalla(valor){
 
-  var numero = document.getElementById("pantalla").value += valor;
+    var numero = document.getElementById("pantalla").value += valor;
 }
 
 function operacion(){
 
   var expMath= document.getElementById("pantalla").value;
-  console.log(expMath);
+//  console.log(expMath);
 
-var res = expMath.split("+");
+//var res = expMath.split("+");
 //console.log(res);
   //console.log(resultado);
-  //console.log(expMath);
+  console.log(expMath);
   for(var i = 0 ; i<expMath.length ; i++){
-    if(expMath[i]==="+"){
+    if(expMath[i] === "+"){
       var add = expMath.split("+");
-      var resultado = parseInt(add[0])+ parseInt(add[1]);
-      console.log(resultado);
-      document.getElementById("resultado").innerHTML= resultado;
-      console.log(res);
+      var resultado =document.getElementById("resultado").value= parseInt(add[0])+ parseInt(add[1]);
+    //  console.log(resultado);
+      //var numero = document.getElementById("pantalla").value += valor;
+
+      //document.getElementById("resultado").innerHTML= resultado;
+      //console.log(res);
     }else if (expMath[i]==="-" ){
       var resta = expMath.split("-");
-      var resultado = parseInt(resta[0])- parseInt(resta[1]);
+      var resultado = parseInt(resta[0]) - parseInt(resta[1]);
       console.log(resultado)
-    }else if (expMath[i]==="*" ){
+    }else if (expMath[i] === "*" ){
       var mul = expMath.split("*");
-      var resultado = parseInt(resta[0])* parseInt(resta[1]);
+      var resultado = parseInt(mul[0]) * parseInt(mul[1]);
 
       console.log(resultado);
 
     }else if (expMath[i]==="/" ){
       var mul = expMath.split("/");
-      var resultado = parseInt(resta[0]) / parseInt(resta[1]);
+      var resultado = parseInt(mul[0]) / parseInt(mul[1]);
 
       console.log(resultado)
   //  cifra.push()
